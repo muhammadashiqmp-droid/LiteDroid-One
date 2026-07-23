52a618c9479c478b62a259ee9792faeece382fe6
Muhammad Ashiq <muhammadashiqmp@gmail.com>
2026-07-23 22:47:41 +0530

    Connect launcher with runtime manager

--- launcher/main.py
+++ launcher/main.py
@@ -1,4 +1,17 @@
 import customtkinter as ctk
+import sys
+import os
+
+
+# Allow importing runtime module
+sys.path.append(
+    os.path.join(
+        os.path.dirname(__file__),
+        ".."
+    )
+)
+
+from runtime.runtime_manager import RuntimeManager
 
 
 # App settings
@@ -11,6 +24,18 @@ app = ctk.CTk()
 
 app.title("LiteDroid One")
 app.geometry("900x600")
+
+
+# Function for runtime button
+def start_runtime():
+
+    runtime = RuntimeManager()
+
+    status = runtime.get_status()
+
+    status_label.configure(
+        text=status
+    )
 
 
 # Title
@@ -33,16 +58,27 @@ subtitle = ctk.CTkLabel(
 subtitle.pack()
 
 
+# Runtime status
+status_label = ctk.CTkLabel(
+    app,
+    text="Runtime Status: Unknown",
+    font=("Arial", 16)
+)
+
+status_label.pack(pady=20)
+
+
 # Button
 button = ctk.CTkButton(
     app,
     text="Start Android Runtime",
     width=250,
-    height=45
+    height=45,
+    command=start_runtime
 )
 
 button.pack(pady=40)
 
 
-# Run
+# Run application
 app.mainloop()
